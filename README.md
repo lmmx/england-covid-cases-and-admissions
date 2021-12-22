214ec2f71785f91364a0e1b915528ae8b20bcdcf
Louis Maddox <louismmx@gmail.com>
2021-12-22 08:36:27 +0000

    Use rolling rates for admissions; add mortality; plot Omicron wave

--- README.md
+++ README.md
@@ -3,19 +3,26 @@
 - [x] Plot of COVID cases and hospital admissions in England
   - Absolute; rolling rates; and percentage of historic peaks
 - [x] Plot of COVID cases and hospital admissions in England alongside COVID variant presence
-  - Overlay of Omicron (SGTF) prevalence; overlay of 'dual epidemics'; detail view of last 6 months
+  - [Overlay of Omicron (SGTF) prevalence](plot_rolling_cases_and_admissions_omicron.py);
+  - [overlay of 'dual epidemics'](plot_rolling_cases_and_admissions_omicron_vs_delta.py);
+  - [detail view of last 6 months](plot_rolling_cases_and_admissions_omicron_vs_delta_last_6_months.py);
+  - [detail view of December 2021 Omicron wave](plot_rr_cases_deaths_admissions_omicron_wave.py)
 
 ![](https://raw.githubusercontent.com/lmmx/england-covid-cases-and-admissions/master/plots/rolling_cases_abs_and_admissions.png)
 ![](https://raw.githubusercontent.com/lmmx/england-covid-cases-and-admissions/master/plots/rolling_cases_percentage_and_admissions.png)
 ![](https://raw.githubusercontent.com/lmmx/england-covid-cases-and-admissions/master/plots/rolling_cases_percentage_and_admissions_with_sgtf.png)
 ![](https://raw.githubusercontent.com/lmmx/england-covid-cases-and-admissions/master/plots/rolling_cases_percentage_and_admissions_omicron_vs_delta.png)
 ![](https://raw.githubusercontent.com/lmmx/england-covid-cases-and-admissions/master/plots/rolling_cases_percentage_and_admissions_omicron_vs_delta_6_months.png)
+![](https://raw.githubusercontent.com/lmmx/england-covid-cases-and-admissions/master/plots/rolling_cases_percentage_and_admissions_omicron_vs_delta_from_dec_2021.png)
 
 - `data/ukhsa_national_covid_case_rates_and_admissions.csv` came from
   [source CSV](https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=E92000001&metric=newAdmissions&metric=newCasesBySpecimenDate&format=csv)
   via [nicfreeman1209 on GitHub](https://github.com/nicfreeman1209/covid-19/blob/main/conv-estimator/conv_estimator_direct.ipynb)
   originally via [Nic Freeman](https://twitter.com/nicfreeman1209/status/1472966382719574023)
   on 21st December 2021
+- `data/ukhsa_national_covid_case_rates_deaths_admissions_rolling_rates.csv` came from 
+  [source CSV](https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=E92000001&metric=newAdmissionsRollingRate&metric=newCasesBySpecimenDateRollingRate&metric=newDeaths28DaysByDeathDateRollingRate&format=csv)
+  and was deliberately chosen to use rolling rates for all metrics
 - `data/ukhsa_sgtf_totalepicurve_2021-12-20-1.csv` came from
   [source CSV](https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1042753/sgtf_totalepicurve_2021-12-20-1.csv)
   via [Omicron daily overview](https://www.gov.uk/government/publications/covid-19-omicron-daily-overview)
--- plot_rr_cases_deaths_admissions_omicron_wave.py
+++ plot_rr_cases_deaths_admissions_omicron_wave.py
@@ -2,19 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime as dt
 
-ukhsa_national_case_csv = "data/ukhsa_national_covid_case_rolling_rates_and_admissions.csv"
+ukhsa_national_rr_csv = "data/ukhsa_national_covid_case_rates_deaths_admissions_rolling_rates.csv"
 ukhsa_omicron_sgtf_csv = "data/ukhsa_sgtf_totalepicurve_2021-12-20-1.csv"
 
-adm_case_df = pd.read_csv(ukhsa_national_case_csv)
+adm_case_df = pd.read_csv(ukhsa_national_rr_csv)
 adm_case_df.date = pd.to_datetime(adm_case_df.date)
-adm_case_df = adm_case_df[adm_case_df.date > dt(2021,6,1)]
 
 sgtf_df = pd.read_csv(ukhsa_omicron_sgtf_csv)
 sgtf_df = sgtf_df[sgtf_df.sgtf == "Cases with SGTF"]
 sgtf_df.drop(columns="sgtf", inplace=True)
 sgtf_df.specimen_date = pd.to_datetime(sgtf_df.specimen_date, format="%d/%m/%Y")
 sgtf_df.set_index("specimen_date", inplace=True)
-sgtf_y_lab = "percentOmicronSGTF"
+sgtf_y_lab = "%OmicronSGTF"
 sgtf_df.rename(columns={"percent": sgtf_y_lab}, inplace=True)
 
 # No admissions figures in this dataset until 18 March 2020
@@ -23,9 +22,11 @@ march_18_2020 = dt(2020, 3, 18)
 november_1_2021 = dt(2021, 11, 1)
 
 case_y_original = "newCasesBySpecimenDateRollingRate"
-case_y_lab = "newCasesBySpecimenDateRollingRatePercentageDec2021Peak"
-adm_y_original = "newAdmissions"
-adm_y_lab = "newAdmissionsPercentageJan2021Peak"
+case_y_lab = case_y_original + "_%Dec2021Peak"
+adm_y_original = "newAdmissionsRollingRate"
+adm_y_lab = adm_y_original + "_%Jan2021Peak"
+mort_y_original = "newDeaths28DaysByDeathDateRollingRate"
+mort_y_lab = mort_y_original + "_%Jan2021Peak"
 adm_case_df = adm_case_df[adm_case_df.date > march_18_2020]
 adm_case_df.set_index("date", inplace=True)
 
@@ -35,25 +36,33 @@ adm_case_df[adm_y_lab] = 100 * adm_case_df[adm_y_original] / peak_adm_jan_2021
 peak_case_dec_2021 = adm_case_df[case_y_original].max()
 adm_case_df[case_y_lab] = 100 * adm_case_df[case_y_original] / peak_case_dec_2021
 
+peak_mort_jan_2021 = adm_case_df[mort_y_original].max()
+adm_case_df[mort_y_lab] = 100 * adm_case_df[mort_y_original] / peak_mort_jan_2021
+
+sgtf_df = sgtf_df[sgtf_df.index > dt(2021,12,1)]
+
 omi_case_df = adm_case_df[[case_y_lab]].join(sgtf_df[[sgtf_y_lab]]).dropna()
-omi_y_lab = "newCasesOmicronRollingRatePercentageDec2021TotalPeak"
-del_y_lab = "newCasesDeltaRollingRatePercentageDec2021TotalPeak"
+omi_y_lab = case_y_lab + "_OMICRON"
+del_y_lab = case_y_lab + "_DELTA"
 omi_case_df[omi_y_lab] = omi_case_df.prod(axis=1) / 100
 omi_case_df[del_y_lab] = omi_case_df[case_y_lab] - omi_case_df[omi_y_lab]
 
+adm_case_df = adm_case_df[adm_case_df.index > dt(2021,12,1)]
+
 def main():
-    plt.figure(figsize=(16, 8), dpi=250)
-    adm_case_df[case_y_lab].plot()
+    plt.figure(figsize=(8, 8), dpi=250)
     adm_case_df[adm_y_lab].plot()
+    adm_case_df[case_y_lab].plot(c="turquoise")
     omi_case_df[omi_y_lab].plot()
     omi_case_df[del_y_lab].plot()
+    adm_case_df[mort_y_lab].plot()
     plt.tight_layout(pad=2)
     from_date = str(adm_case_df.index.min()).split(" ")[0]
     to_date = str(adm_case_df.index.max()).split(" ")[0]
     plt.xlabel(f"Date (from {from_date} to {to_date})")
     plt.ylabel("% of historic peak (cases/admissions) OR % of SGTF (Omicron)")
     plt.legend()
-    #plt.show()
-    plt.savefig("plots/rolling_cases_percentage_and_admissions_omicron_vs_delta_6_months.png")
+    plt.show()
+    #plt.savefig("plots/rolling_cases_percentage_deaths_admissions_omicron_vs_delta_from_dec_2021.png")
 
 #main()
